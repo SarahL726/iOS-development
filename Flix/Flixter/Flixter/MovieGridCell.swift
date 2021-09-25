4db7a9691c5ec4b940c2dc9289204727dd73e1d6
SarahL726 <SarahL726@github.com>
2021-09-24 23:54:16 -0400

    Flix Part 2

---
+++ Flix/Flixter/Flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by Sarah Li on 9/24/21.
+//  Copyright © 2021 Xinyue Li. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
